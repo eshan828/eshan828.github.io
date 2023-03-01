d3b630347850f38f32e88026e81da8671df277c3
eshan828 <126671349+eshan828@users.noreply.github.com>
2023-03-01 20:23:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ scanButton.addEventListener("click", async () => {
   document.write("<p>User clicked scan button<p>");
 
   try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    document.write("> Scan started");
+  //   const ndef = new NDEFReader();
+  //   await ndef.scan();
+  //   document.write("> Scan started");
 
-    ndef.addEventListener("readingerror", () => {
-      document.write("Argh! Cannot read data from the NFC tag. Try another one?");
-    });
+  //   ndef.addEventListener("readingerror", () => {
+  //     document.write("Argh! Cannot read data from the NFC tag. Try another one?");
+  //   });
 
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      document.write(`> Serial Number: ${serialNumber}`);
-      // document.write(`> Records: (${message.records.length})`);
-      //  document.write(`> record type: (${message.records[0].recordType})`);
-      //  document.write(`> data: (${message.records[0].data})`);
-      // document.write(`> data: (${message.records[0]})`);
-      readTextRecord(message.records[0])
-  const textDecoder = new TextDecoder(message.records[0].encoding);
-  document.write(`Text: ${textDecoder.decode(message.records[0].data)} (${message.records[0].lang})`);
+  //   ndef.addEventListener("reading", ({ message, serialNumber }) => {
+  //     document.write(`> Serial Number: ${serialNumber}`);
+  //     document.write(`> Records: (${message.records.length})`);
+  //      document.write(`> record type: (${message.records[0].recordType})`);
+  //      document.write(`> data: (${message.records[0].data})`);
+  //     document.write(`> data: (${message.records[0]})`);
+  //     readTextRecord(message.records[0])
+  // const textDecoder = new TextDecoder(message.records[0].encoding);
+  // document.write(`Text: ${textDecoder.decode(message.records[0].data)} (${message.records[0].lang})`);
      
-    });
-  
+  //   });
+  const ndef = new NDEFReader();
+await ndef.scan();
+ndef.onreading = (event) => {
+  const decoder = new TextDecoder();
+  for (const record of event.message.records) {
+    if (record.mediaType === "application/json") {
+      const json = JSON.parse(decoder.decode(record.data));
+      const article = /^[aeio]/i.test(json.title) ? "an" : "a";
+      document.write(`${json.name} is ${article} ${json.title}`);
+    }
+  }
+};
     
   } catch (error) {
     document.write("Argh! " + error);
