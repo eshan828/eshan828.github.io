945b08109623ea4200e942581e068e3676000270
eshan828 <126671349+eshan828@users.noreply.github.com>
2023-03-01 19:48:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
 scanButton.addEventListener("click", async () => {
-  console.log("User clicked scan button");
+  document.write("<p>User clicked scan button<p>");
 
   try {
     const ndef = new NDEFReader();
     await ndef.scan();
-    document.getElementById("output").innerText="> Scan started";
-   
+    document.write("> Scan started");
 
     ndef.addEventListener("readingerror", () => {
-      document.getElementById("output").innerText="Argh! Cannot read data from the NFC tag. Try another one?";
+      document.write("Argh! Cannot read data from the NFC tag. Try another one?");
     });
 
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
-        var text=document.getElementById("output").innerText;
-      text=text+`> Serial Number: ${serialNumber}`;
-      text=text+`> Records: (${message})`;
-       text=text+ JSON.stringify(message);
-        document.getElementById("output").innerText=text;
-
-      // log(`> Serial Number: ${serialNumber}`);
-      // log(`> Records: (${message.records.length})`);
-     var rec="";
-        for (const rec of message.records) {
-            record+=`Record type:  ${record.recordType}`+ "\n" + `MIME type:    ${record.mediaType}` + "\n" + `Record id:    ${record.id}`;
-//     console.log(`Record type:  ${record.recordType}`);
-//     console.log(`MIME type:    ${record.mediaType}`);
-//     console.log(`Record id:    ${record.id}`);
-  switch (record.recordType) {
-    case "text":
-      // TODO: Read text record with record data, lang, and encoding.
-      break;
-    case "url":
-      // TODO: Read URL record with record data.
-      break;
-    default:
-    // TODO: Handle other records with record data.
-  }
-           
-}
-         document.getElementById("output2").innerText=rec;
+      document.write(`> Serial Number: ${serialNumber}`);
+      document.write(`> Records: (${message.records.length})`);
+       document.write(`> record type: (${message.records.recordType})`);
+       document.write(`> data: (${message.records.data})`);
     });
+  
+    
   } catch (error) {
-    console.log("Argh! " + error);
-        document.getElementById("output").innerText=`Argh!  + ${error}`;
+    document.write("Argh! " + error);
   }
 });
 
-//   writeButton.addEventListener("click", async () => {
-//     console.log("User clicked write button");
+writeButton.addEventListener("click", async () => {
+  log("User clicked write button");
 
-//     try {
-//       const ndef = new NDEFReader();
-//       await ndef.write("Hello world!");
-//       console.log("> Message written");
-//     } catch (error) {
-//       console.log("Argh! " + error);
-//     }
-//   });
+  try {
+    const ndef = new NDEFReader();
+    await ndef.write("Hello world!");
+    log("> Message written");
+  } catch (error) {
+    log("Argh! " + error);
+  }
+});
 
 makeReadOnlyButton.addEventListener("click", async () => {
-  console.log("User clicked make read-only button");
+  log("User clicked make read-only button");
 
   try {
     const ndef = new NDEFReader();
     await ndef.makeReadOnly();
-    console.log("> NFC tag has been made permanently read-only");
+    log("> NFC tag has been made permanently read-only");
   } catch (error) {
-    console.log("Argh! " + error);
+    log("Argh! " + error);
   }
 });
